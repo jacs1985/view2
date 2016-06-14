242888bae2f4970b080c79e9cbadffbc8407d00b
fulloa <felipe.ulload@alumnos.uv.cl>
2016-06-14 18:10:39 -0400

    login

--- scripts/loginController.js
+++ scripts/loginController.js
@@ -1,6 +1,9 @@
-app.controller('loginController', ['$scope', function($scope){
+app.controller('loginController', ['$scope', function($scope,$location){
 
 	$scope.ingresar = function (user) {
 		console.log(user);
+		if (user.email === "admin@admin.com" && user.pass === "admin") {
+			console.log("correcto");
+		}
 	}
 }])
